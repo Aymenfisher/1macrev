490f325fdb6290675652d2bd31d689d932525b25
Aymenfisher <fisheraymen@gmail.com>
2018-03-07 17:01:55 +0100

    now atm.py can work with multiple banks and atms

--- atm.py
+++ atm.py
@@ -1,23 +1,24 @@
-balance=500
-def atm(money,request):
-	given=money-request
-	available_cash=[100,50,10,5,4,3,2,1]
-	if request<=0 or request>money:
-		print  "you entered invalid number , or i cant give you all this money boi !!"
-		return
-	index=0	
-	while request>0:
-		if request>=available_cash[index]:
-			print "Give",available_cash[index]
-			request-=available_cash[index]
-		else:
-			index+=1
-	return given		
-			
+class ATM(object):
+  def __init__(self,money,bank_name):
+    self.money=money
+    self.bank_name=bank_name
+  def withdraw(self,request):
+    print "Welcome to ",self.bank_name
+    print "Current Balance ",self.money
+    print "=================================="
 
-
-
-
-
-
-			
+    given=self.money-request
+    available_cash=[100,50,10,5,4,3,2,1]
+    if request<=0 or request>self.money:
+      print "you entered invalid , or i cant give you all that money boi !!"
+      print "=================================="
+      return
+    index=0
+    while request>0:
+      if request>=available_cash[index]:
+        print "Give",available_cash[index]
+        request-=available_cash[index]
+      else:
+        index+=1
+    self.money=given
+    print "=================================="
