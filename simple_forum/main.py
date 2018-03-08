e5be216490a86f50874b6080d9383e00199f3002
Aymenfisher <fisheraymen@gmail.com>
2018-03-08 16:40:11 +0100

    created member & post stores

--- simple_forum/main.py
+++ simple_forum/main.py
@@ -5,3 +5,13 @@ post1=Post("help me","what is python?")
 post2=Post("Python quizz","write python code that calculates days between dates")
 post3=Post("diffirence?","whats the diffirence between python 2 & python 3")
 
+member_store=MemberStore()
+post_store=PostStore()
+
+member_store.add(member2.name)
+member_store.add(member1.name)
+post_store.add(post1.title)
+post_store.add(post2.title)
+post_store.add(post3.title)
+member_store.get_all()
+post_store.get_all()
--- simple_forum/models.py
+++ simple_forum/models.py
@@ -6,3 +6,18 @@ class Post(object):
 	def __init__(self,title,content):
 		self.title=title
 		self.content=content
+class MemberStore(object):
+	members=[]
+	def add(self,member):
+		MemberStore.members.append(member)
+	def get_all(self):
+		print MemberStore.members
+class PostStore(object):
+	posts=[]
+	def add(self,post):
+		PostStore.posts.append(post)
+	def get_all(self):
+		print PostStore.posts
+		
+
+		
