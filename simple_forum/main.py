87febb1bc54ca421ab791b55573a0861616e97a7
Aymenfisher <fisheraymen@gmail.com>
2018-03-08 14:55:52 +0100

    simple forum, contains member(name & age) and post(title and content)

---
+++ simple_forum/main.py
@@ -0,0 +1,7 @@
+from models import *
+member1=Member("Aymen",20)
+member2=Member("Fisher",21)
+post1=Post("help me","what is python?")
+post2=Post("Python quizz","write python code that calculates days between dates")
+post3=Post("diffirence?","whats the diffirence between python 2 & python 3")
+
